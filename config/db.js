fcb46039d200f187b53c6a44e9aadb40163df81b
Nitin Surya Imadabathuni <65825453+initinsurya@users.noreply.github.com>
2023-02-10 15:43:39 -0500

    Mongo URI Change

--- config/db.js
+++ config/db.js
@@ -1,6 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const MONGO_URI =
+  "mongodb+srv://nitinsurya:nitinsurya@cluster0.eqez64u.mongodb.net/test";
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI, {
+  const conn = await mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
